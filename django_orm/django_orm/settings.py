4d9b9d380d8161381b001c682ac5f853ac338f93
desert71 <amkazennov99@gmail.com>
2023-12-11 14:59:57 +0300

    add PostgreSQL

--- django_orm/django_orm/settings.py
+++ django_orm/django_orm/settings.py
@@ -73,10 +73,20 @@ WSGI_APPLICATION = 'django_orm.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'hr',
+        'USER': 'hr',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
