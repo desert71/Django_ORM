c845fd137d7b538d3b85c08e2ae2ccc3701b0fef
desert71 <amkazennov99@gmail.com>
2023-12-13 14:39:58 +0300

    modify models.py

--- django_orm/hr/models.py
+++ django_orm/hr/models.py
@@ -8,11 +8,19 @@ class Contact(models.Model):
 
     def __str__(self) -> str:
         return self.phone
+    
+class Department(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(null=True, blank=True)
+
+    def __str__(self) -> str:
+        return self.name
 
 class Employee(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     contact = models.OneToOneField(Contact, on_delete=models.CASCADE, null=True)
+    department = models.ForeignKey(Department, on_delete=models.CASCADE, default=None)
 
     def __str__(self) -> str:
         return f'{self.first_name} {self.last_name}'
