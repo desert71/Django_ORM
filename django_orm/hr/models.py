bfd54b1f4d45bfc7f2542f6e30c74d0fd2b8d860
desert71 <amkazennov99@gmail.com>
2023-12-13 14:46:34 +0300

    modify models.py

--- django_orm/hr/models.py
+++ django_orm/hr/models.py
@@ -2,6 +2,13 @@ from django.db import models
 
 # Create your models here.
 
+class Compensation(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return self.name
+
+
 class Contact(models.Model):
     phone = models.CharField(max_length=50, unique=True)
     address = models.CharField(max_length=200)
@@ -9,6 +16,7 @@ class Contact(models.Model):
     def __str__(self) -> str:
         return self.phone
     
+
 class Department(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField(null=True, blank=True)
@@ -16,11 +24,13 @@ class Department(models.Model):
     def __str__(self) -> str:
         return self.name
 
+
 class Employee(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     contact = models.OneToOneField(Contact, on_delete=models.CASCADE, null=True)
     department = models.ForeignKey(Department, on_delete=models.CASCADE, default=None)
+    compensations = models.ManyToManyField(Compensation)
 
     def __str__(self) -> str:
         return f'{self.first_name} {self.last_name}'
